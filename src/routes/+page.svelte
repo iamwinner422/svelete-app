271612bd46434a79afe6c45f8d034cd2e2f71163
iamwinner422 <iamwinner422@gmail.com>
2024-12-07 14:39:15 +0000

    Sheeh

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,34 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    interface Todo {
+        text: string;
+        done: boolean
+    }
+    let todos: Array<Todo> = [
+        {text: 'By some milk', done: false},
+        {text: 'By some bread', done: false},
+        {text: 'By Google', done: true},
+        {text: 'Go to the moon🚀', done: true}
+    ];
+    let newtodo: string = '';
+
+    function addTodo(): void {
+        todos = [...todos, {text: newtodo, done: false}]
+        newtodo = '';
+    }
+
+    function removeTodo(index: number): void {
+        todos = todos.filter((_, i) => i !== index);
+    }
+</script>
+
+{#each todos as todo, i}
+    <p>
+        <input type="checkbox" name="todo-{i}" id="todo-{i}" bind:checked={todo.done}/>
+        {i+1}. {todo.text}
+        <button type="button" on:click|preventDefault={() => removeTodo(i)}>x</button>
+    </p>
+{/each}
+<form action="#" on:submit|preventDefault={addTodo}>
+    <input type="text" name="todo" id="todo" bind:value={newtodo}/>
+    <button type="submit">+</button>
+</form>
